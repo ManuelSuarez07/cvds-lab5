b0cc67d22d82894656061ff29ac27394b6206218
ManuelSuarez07 <manuel.suarez.e09@gmail.com>
2024-02-25 19:13:52 -0500

    Punto 3

---
+++ src/test/java/com/example/PartIII/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PartIII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
